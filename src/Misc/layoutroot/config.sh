1ed17a0c750e60aee09a86d3b2d5a4cab1c7676f
Joao Moreno <mail@joaomoreno.com>
2016-08-30 23:48:40 +0200

    remove sudo warnings

--- src/Misc/layoutroot/config.sh
+++ src/Misc/layoutroot/config.sh
@@ -1,11 +1,6 @@
 user_id=`id -u`
 
 # we want to snapshot the environment of the config user
-if [ $user_id -eq 0 ]; then
-    echo "Must not run with sudo"
-    exit 1
-fi
-
 source ./env.sh
 
 if [[ "$1" == "remove" ]]; then
--- src/Misc/layoutroot/run.sh
+++ src/Misc/layoutroot/run.sh
@@ -1,10 +1,5 @@
 user_id=`id -u`
 
-if [ $user_id -eq 0 ]; then
-    echo "Must not run interactively with sudo"
-    exit 1
-fi
-
 if [ ! -f .agent ]; then
     echo "Must configure first. Run ./config.sh"
     exit 1
